a4c5778bcafdd3e5283f08867018a13dccdab730
Wokoro Douye Samuel <wokorosamuel@yahoo.com>
2023-08-07 18:16:47 +0100

    feature: added api calls

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+(() => {
+    const authenticated = window.localStorage.getItem('user');
+
+    console.log('AUTHENTICATED: ', authenticated)
+
+    const home = document.querySelector('#home')
+    const messages = document.querySelector('#messages')
+    const share = document.querySelector('#share')
+    const log = document.querySelector('#log')
+
+    if (authenticated) {
+        if (home) home.style.display = 'inline'
+        if (messages) messages.style.display = 'inline'
+        if (share) share.style.display = 'inline'
+
+        if (log) {
+            log.href = '#'
+            log.textContent = 'log out'
+            log.addEventListener('click', () => {
+                window.localStorage.removeItem('user')
+                window.location.href = './home.html'
+            })
+        }
+    } else {
+        messages.style.display = 'none'
+        share.style.display = 'none'
+        log.style.display = 'inline'
+    }
+})()
